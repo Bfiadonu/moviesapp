7212829ead07fa0360bc892413259ddfcd068c51
bfiadonu <bfiadonu@gmail.com>
2020-08-25 09:52:25 -0400

    moviecommit

---
+++ Skeletons/rcc.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function Footer () {
+  return (
+  <div>
+    
+    </div>
+  )
+}
+
+export default Footer
+
+
+import React, { Component } from 'react';
+
+export class SearchForm extends Component {
+  render() {
+    return (
+      
+    )
+  }
+}
+export default SearchForm
---
+++ src/APIKey.js
@@ -0,0 +1,3 @@
+module.exports = {
+  APIKey: '6f85a3d8'
+};
